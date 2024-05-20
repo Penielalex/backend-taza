a93a8f79b61464f3d136d1b17747fe812cbe1852
Peniel Alemayehu <peniel12alex@gmail.com>
2024-05-20 18:20:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -52,7 +52,7 @@ async function ensureRolesExist() {
 
 
 
-db.sequelize.sync({force:true}).then(() =>{
+db.sequelize.sync().then(() =>{
     ensureRolesExist().then(() => {
         app.listen(PORT, () => {
           console.log('App listening on port ' + PORT);
