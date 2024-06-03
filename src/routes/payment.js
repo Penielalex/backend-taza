6862f98e9f88272b8c0f79960c987ec6ff7eb46b
Penielalex <101531202+Penielalex@users.noreply.github.com>
2024-06-03 20:01:56 +0300

    other

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -21,9 +21,9 @@ router.post('/create-payment', async (req, res) => {
         amount,
         currency: 'ETB',
         first_name: existingUser.firstName,
-        last_name: existingUser.last_name,
+        last_name: existingUser.lastName,
         phone_number: existingUser.phoneNo,
-        callback_url:"https://backend-taza.onrender.com/verify-payment",
+        callback_url:"https://backend-taza.onrender.com/verify-payment/${tx_ref}",
         tx_ref: tx_ref,
         
       }, {
@@ -56,49 +56,35 @@ router.post('/create-payment', async (req, res) => {
     }
   });
 
-router.post('/verify-payment', async (req, res) => {
-  try {
+  router.post('/verify-payment/:tx_ref', async (req, res) => {
+    const { tx_ref } = req.params;
 
-    const hash = crypto
-      .createHmac("sha256","beb728459913ac239280a12b1048a39a7bd7e6c5852f61e26b638e8004235c24")
-      .update(JSON.stringify(req.body))
-      .digest("hex");
-      if (hash == req.headers["x-chapa-signature"]) {
-        // Retrieve the request's body
-        const event = req.body;
-        console.log(event);
-  
-        const { tx_ref, status } = event;
-        if (status == "success" && tx_ref) {
-          // hit the verify endpoint to make sure a transaction with the given
-          // tx_ref was successful
-          const response = await axios.get(
-            `https://api.chapa.co/v1/transaction/verify/${tx_ref}`,
-  
-            {
-              headers: {
-                Authorization: "Bearer " + process.env.CHAPA_KEY,
-              },
+    try {
+        const response = await axios.get(`https://api.chapa.co/v1/transaction/verify/${tx_ref}`, {
+            headers: {
+                Authorization: 'Bearer CHASECK_TEST-VNHcYDRNqJ4LXi0ADJ7gWophtt9qBsHV',
             }
-          );
-          if (response.status == 200) {
-            // if successful find the order
-            if (response.data["status"] == "success") {
-              const chapaPaymentId = tx_ref;
-              const pay = await payment.findOne({ where: { chapaPaymentId } });
-              pay.status = response.data.data.status;
-              await pay.save();
-      
-              if (pay.status === 'success') {
-                  const User = await user.findByPk(pay.userId);
-                  User.paid = true;
-                  await User.save();
-              }
-              res.json({ success: true });
-            }
-          }
+        });
+        const chapaPaymentId = tx_ref;
+
+        const pay = await payment.findOne({ where: { chapaPaymentId } });
+        if (!pay) {
+            return res.status(404).json({ error: 'Payment not found' });
         }
-      }
+
+        pay.status = response.data.data.status;
+        await pay.save();
+
+        if (pay.status === 'success') {
+            const existinguser = await user.findByPk(pay.userId);
+            if (!user) {
+                return res.status(404).json({ error: 'User not found' });
+            }
+            existinguser.paid = true;
+            await existinguser.save();
+        }
+
+        res.json({ success: true });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Failed to verify payment' });
